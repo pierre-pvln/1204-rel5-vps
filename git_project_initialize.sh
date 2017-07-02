bc2bfcb2769d346538474b7b9ff2d77fe0b7ab01
Pierre <pierre@pvln.nl>
2017-07-02 16:03:11 +0200

    updated comments

--- git_project_initialize.sh
+++ git_project_initialize.sh
@@ -1,9 +1,12 @@
-:: 
-:: Auteur:	Pierre Veelen
-:: Datum:	2015-05-07
-::
-:: Shell script om GIT te initialiseren met de juiste parameters.
-::
+#!/bin/bash
+# Name:     git_project_initialize.sh
+# Purpose:  A shell script to initialize git.
+# Author:   pierre@pvln.nl
+# Revision: 2017 07 02 - initial version
+# 
+# commandline:
+# ./git_project_initialize.sh
+# ---------------------------------------------------
 
 git config --global user.name Pierre Veelen
 git config --global user.email pierre@pvln.nl
